d9e0f0be1797bae25095682d0ed260b4b9811376
Robin Bette <robinbette@hotmail.com>
2021-07-26 15:10:57 +0200

    add browser window

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+const { app, BrowserWindow } = require("electron");
+
+function createWindow() {
+  const window = new BrowserWindow({
+    width: 800,
+    height: 600,
+    backgroundColor: "white",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  window.loadFile("index.html");
+}
+
+app.whenReady().then(createWindow);
