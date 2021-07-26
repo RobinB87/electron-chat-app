2747a81f81399c79006e5d3d1914e4321cb8b5c0
Robin Bette <robinbette@hotmail.com>
2021-07-26 16:22:16 +0200

    create notification

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Notification } = require("electron");
 
 function createWindow() {
   const window = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     backgroundColor: "white",
     webPreferences: {
       nodeIntegration: true,
@@ -11,6 +11,25 @@ function createWindow() {
   });
 
   window.loadFile("index.html");
+  window.webContents.openDevTools();
 }
 
-app.whenReady().then(createWindow);
+app.whenReady().then(() => {
+  createWindow();
+  const notification = new Notification({ title: "Hello World", body: "My test message" });
+  notification.show();
+});
+
+app.on("window-all-closed", () => {
+  // darwin is mac
+  // process.platform !== "win32"
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
